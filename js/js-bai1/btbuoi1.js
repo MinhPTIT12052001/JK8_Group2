94166708b2b24be73fb0aa709598fc2187e69aee
phamtranduc <phamtranducc@gmail.com>
2024-01-03 10:39:26 +0700

    bai tap buoi 1

---
+++ js/js-bai1/btbuoi1.js
@@ -0,0 +1,7 @@
+let name = prompt("Nhập họ và tên");
+let age = prompt("Nhập tuổi:");
+let phoneNumber = prompt("Nhập số điện thoại:");
+
+console.log(name);
+console.log(age);
+console.log(phoneNumber);
